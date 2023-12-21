41a967abc1d748874a4ca8cb7ced663ee0e22681
MAJ-TT <zagzig186@gmail.com>
2023-12-21 03:15:19 -0400

    Updated/Reversed

--- src/events/client/clientReady.js
+++ src/events/client/clientReady.js
@@ -1,63 +1,52 @@
-const Discord = require("discord.js");
-const chalk = require("chalk");
-const { random } = require("mathjs");
+const Discord = require('discord.js');
+const chalk = require('chalk');
+const { random } = require('mathjs');
 
 module.exports = async (client) => {
-  const startLogs = new Discord.WebhookClient({
-    id: client.webhooks.startLogs.id,
-    token: client.webhooks.startLogs.token,
-  });
+    const startLogs = new Discord.WebhookClient({
+        id: client.webhooks.startLogs.id,
+        token: client.webhooks.startLogs.token,
+    });
 
-  console.log(`\u001b[0m`);
-  console.log(
-    chalk.blue(chalk.bold(`System`)),
-    chalk.white(`>>`),
-    chalk.red(`Shard #${client.shard.ids[0] + 1}`),
-    chalk.green(`is ready!`)
-  );
-  console.log(
-    chalk.blue(chalk.bold(`Bot`)),
-    chalk.white(`>>`),
-    chalk.green(`Started on`),
-    chalk.red(`${client.guilds.cache.size}`),
-    chalk.green(`servers!`)
-  );
+    console.log(`\u001b[0m`);
+    console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), chalk.red(`Shard #${client.shard.ids[0] + 1}`), chalk.green(`is ready!`))
+    console.log(chalk.blue(chalk.bold(`Bot`)), (chalk.white(`>>`)), chalk.green(`Started on`), chalk.red(`${client.guilds.cache.size}`), chalk.green(`servers!`))
 
-  let embed = new Discord.EmbedBuilder()
-    .setTitle(`🆙・Finishing shard`)
-    .setDescription(`A shard just finished`)
-    .addFields(
-      {
-        name: "🆔┆ID",
-        value: `${client.shard.ids[0] + 1}/${client.options.shardCount}`,
-        inline: true,
-      },
-      { name: "┆State", value: `Ready`, inline: true }
-    )
-    .setColor(client.config.colors.normal);
-  startLogs.send({
-    username: "Bot Logs",
-    embeds: [embed],
-  });
+    let embed = new Discord.EmbedBuilder()
+        .setTitle(`🆙・Finishing shard`)
+        .setDescription(`A shard just finished`)
+        .addFields(
+            { name: "🆔┆ID", value: `${client.shard.ids[0] + 1}/${client.options.shardCount}`, inline: true },
+            { name: "📃┆State", value: `Ready`, inline: true },
+        )
+        .setColor(client.config.colors.normal)
+    startLogs.send({
+        username: 'Bot Logs',
+        embeds: [embed],
+    });
 
-  setInterval(async function () {
-    try {
-      let totalGuilds; // Declare totalGuilds here
-      if (client.guilds.cache.size !== totalGuilds) {
-        return; // Wait for guilds to load fully
-      }
+    setInterval(async function () {
+        const promises = [
+            client.shard.fetchClientValues('guilds.cache.size'),
+        ];
+        return Promise.all(promises)
+            .then(results => {
+                const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+                let statuttext;
+                if (process.env.DISCORD_STATUS) {
+                    statuttext = process.env.DISCORD_STATUS.split(', ');
+                } else {
+                    statuttext = [
+                        `/help`,
+                        `${totalGuilds} servers`,
+                        `discord.gg/NewJins`,
+                        `with MAJ in VS Code`,
+                    ];
+                }
+                const randomText = statuttext[Math.floor(Math.random() * statuttext.length)];
+                client.user.setPresence({ activities: [{ name: randomText, type: Discord.ActivityType.Playing }], status: 'idle' });
+            })
+    }, 20000)
 
-      const promises = [client.shard.fetchClientValues("guilds.cache.size")];
-
-      const results = await Promise.all(promises);
-      totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-      let totalUsers = 0;
-
-      // ... rest of your code
-    } catch (error) {
-      console.error("Error fetching user count:", error);
-    }
-  }, 20000);
-
-  client.player.init(client.user.id);
-};
+    client.player.init(client.user.id);
+}
